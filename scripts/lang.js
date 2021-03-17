ea7ede90f08bde28f518b296b086f57b41f6237c
mathiasiriarte <mathias.iriarte@hotmail.com>
2021-03-16 21:35:21 -0300

    added event listener and script on html

--- scripts/lang.js
+++ scripts/lang.js
@@ -1,2 +1,8 @@
+//GET IMAGES ID
 const eng = document.getElementById("langEng");
 const es = document.getElementById("langEs");
+
+eng.addEventListener('click', ()=>{
+    
+    console.log("click on uk flag");
+})
