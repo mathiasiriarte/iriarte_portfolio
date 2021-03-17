23b714df9522674c7bcd5a263134944d95ceb2f7
mathiasiriarte <mathias.iriarte@hotmail.com>
2021-03-16 21:37:17 -0300

    added events

--- scripts/lang.js
+++ scripts/lang.js
@@ -3,6 +3,11 @@ const eng = document.getElementById("langEng");
 const es = document.getElementById("langEs");
 
 eng.addEventListener('click', ()=>{
-    
+    window.location.href='/indexEng.html';
+    console.log("click on uk flag");
+})
+
+es.addEventListener('click', ()=>{
+    window.location.href='/index.html';
     console.log("click on uk flag");
 })
