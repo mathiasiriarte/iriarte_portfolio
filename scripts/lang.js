2ea2976dbb1fa9d2bdf7b0f43775a07c5450b409
mathiasiriarte <mathias.iriarte@hotmail.com>
2021-03-16 21:38:10 -0300

    added comment on lang js

--- scripts/lang.js
+++ scripts/lang.js
@@ -2,6 +2,7 @@
 const eng = document.getElementById("langEng");
 const es = document.getElementById("langEs");
 
+//NAVIGATION EVENT
 eng.addEventListener('click', ()=>{
     window.location.href='/indexEng.html';
     console.log("click on uk flag");
